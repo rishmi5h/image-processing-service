3390dedd04f462e3dda734159ef5305bf498c9f3
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-25 02:37:45 +0530

    Create User.java

---
+++ src/main/java/com/imageprocessing/entities/User.java
@@ -0,0 +1,34 @@
+package com.imageprocessing.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+
+@Table(name = "t_user")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
