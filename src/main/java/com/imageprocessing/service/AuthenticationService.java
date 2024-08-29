bad17822fd43fa89881a855192b63c3877c44844
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-30 04:58:34 +0530

    Create AuthenticationService.java

---
+++ src/main/java/com/imageprocessing/service/AuthenticationService.java
@@ -0,0 +1,42 @@
+package com.imageprocessing.service;
+
+import com.imageprocessing.entities.User;
+import com.imageprocessing.model.LoginInfo;
+import com.imageprocessing.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public String register(LoginInfo request) {
+        var user = User.builder()
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return jwtToken;
+    }
+
+    public String authenticate(LoginInfo request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByUsername(request.getUsername())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return jwtToken;
+    }
+}
