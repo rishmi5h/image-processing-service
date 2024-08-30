6c2b3489cade308b07fa97864e53b3387d90b182
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-31 05:08:11 +0530

    Update ImageProcessingController.java

--- src/main/java/com/imageprocessing/controller/ImageProcessingController.java
+++ src/main/java/com/imageprocessing/controller/ImageProcessingController.java
@@ -1,10 +1,11 @@
 package com.imageprocessing.controller;
 
-import com.imageprocessing.model.AuthInfoRequest;
+import com.imageprocessing.model.AuthInfo;
 import com.imageprocessing.model.AuthInfoResponse;
 import com.imageprocessing.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,24 +21,20 @@ public class ImageProcessingController {
     private final AuthenticationService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthInfoResponse> register(@RequestBody AuthInfoRequest request) {
+    public ResponseEntity<AuthInfo> register(@RequestBody AuthInfo request) {
         log.info("Register Info: {}", request.getUsername());
-        AuthInfoResponse response = new AuthInfoResponse();
-        response.setUsername(request.getUsername());
-        response.setPassword(request.getPassword());
-        response.setJwt(authService.register(request));
-        log.info("Register Response: {}", response.getUsername());
-        return ResponseEntity.ok(response);
+        String jwt = authService.register(request);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + jwt);
+        return ResponseEntity.ok().headers(headers).body(request);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthInfoResponse> login(@RequestBody AuthInfoRequest request) {
+    public ResponseEntity<AuthInfo> login(@RequestBody AuthInfo request) {
         log.info("Login Info: {}", request.getUsername());
-        AuthInfoResponse response = new AuthInfoResponse();
-        response.setUsername(request.getUsername());
-        response.setPassword(request.getPassword());
-        response.setJwt(authService.login(request));
-        log.info("Login Response: {}", response.getUsername());
-        return ResponseEntity.ok(response);
+        String jwt = authService.login(request);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + jwt);
+        return ResponseEntity.ok().headers(headers).body(request);
     }
 }
