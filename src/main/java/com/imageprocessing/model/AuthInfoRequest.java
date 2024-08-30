fead66d1feb97182b7e6902791d901b50bf54645
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-31 04:54:38 +0530

    Create AuthInfoRequest.java

---
+++ src/main/java/com/imageprocessing/model/AuthInfoRequest.java
@@ -0,0 +1,13 @@
+package com.imageprocessing.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class AuthInfoRequest {
+    public String username;
+    public String password;
+}
