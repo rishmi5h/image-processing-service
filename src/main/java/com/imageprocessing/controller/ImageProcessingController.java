a766783423030f6b6bf9a35ae919b571b902ab5a
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-01 19:02:48 +0530

    Update ImageProcessingController.java

--- src/main/java/com/imageprocessing/controller/ImageProcessingController.java
+++ src/main/java/com/imageprocessing/controller/ImageProcessingController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173, https://imagery.rishmi5h.com")
 @RequiredArgsConstructor
 @Slf4j
 public class ImageProcessingController {
