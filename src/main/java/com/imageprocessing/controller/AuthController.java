0331bb7d595ad46af252a3e28efeb0130deace89
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-02 03:04:42 +0530

    rename: auth controller

--- src/main/java/com/imageprocessing/controller/AuthController.java
+++ src/main/java/com/imageprocessing/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:5173, https://imagery.rishmi5h.com")
 @RequiredArgsConstructor
 @Slf4j
-public class ImageProcessingController {
+public class AuthController {
 
     private final AuthenticationService authService;
 
