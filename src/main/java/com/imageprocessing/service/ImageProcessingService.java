508de1cdf5c7295a068da004eadd7f92192ee49f
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-06 05:18:18 +0530

    Update ImageProcessingService.java

--- src/main/java/com/imageprocessing/service/ImageProcessingService.java
+++ src/main/java/com/imageprocessing/service/ImageProcessingService.java
@@ -3,7 +3,18 @@ package com.imageprocessing.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.core.sync.RequestBody;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -11,28 +22,31 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ImageProcessingService {
 
-//    private final AmazonS3 amazonS3Client;
-//
-//    @Value("${aws.s3.bucket}")
-//    private String bucketName;
-//
-//    public Map<String, String> uploadImage(MultipartFile file) throws IOException {
-//        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
-//        ObjectMetadata metadata = new ObjectMetadata();
-//        metadata.setContentType(file.getContentType());
-//        metadata.setContentLength(file.getSize());
-//
-//        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, file.getInputStream(), metadata);
-//        amazonS3Client.putObject(putObjectRequest);
-//
-//        String fileUrl = amazonS3Client.getUrl(bucketName, fileName).toString();
-//
-//        Map<String, String> response = new HashMap<>();
-//        response.put("fileName", fileName);
-//        response.put("fileUrl", fileUrl);
-//        response.put("fileType", file.getContentType());
-//        response.put("size", String.valueOf(file.getSize()));
-//
-//        return response;
-//    }
+    private final S3Client amazonS3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    public Map<String, String> uploadImage(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .contentLength(file.getSize())
+                .build();
+
+        PutObjectResponse putObjectResponse = amazonS3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        String fileUrl = "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
+
+        Map<String, String> response = new HashMap<>();
+        response.put("fileName", fileName);
+        response.put("fileUrl", fileUrl);
+        response.put("fileType", file.getContentType());
+        response.put("size", String.valueOf(file.getSize()));
+
+        return response;
+    }
 }
