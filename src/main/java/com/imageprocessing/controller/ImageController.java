1099e434fb33486842157ad7d94eb81396087aab
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-06 05:18:16 +0530

    Update ImageController.java

--- src/main/java/com/imageprocessing/controller/ImageController.java
+++ src/main/java/com/imageprocessing/controller/ImageController.java
@@ -4,9 +4,13 @@ import com.imageprocessing.service.ImageProcessingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/images")
 @CrossOrigin(origins = "http://localhost:5173, https://imagery.rishmi5h.com")
 @RequiredArgsConstructor
 @Slf4j
@@ -14,28 +18,29 @@ public class ImageController {
 
     private final ImageProcessingService imageProcessingService;
 
-//    @PostMapping("/")
-//    public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("file") MultipartFile file) {
-//        try {
-//            Map<String, String> response = imageProcessingService.uploadImage(file);
-//            return ResponseEntity.ok(response);
-//        } catch (IOException e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Failed to upload image"));
-//        }
-//    }
+    @PostMapping("/images")
+    public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            Map<String, String> response = imageProcessingService.uploadImage(file);
+            return ResponseEntity.ok(response);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error", "Failed to upload image: " + e.getMessage()));
+        }
+    }
 
-    @PostMapping("/:id/transform")
+    @PostMapping("/images/:id/transform")
     public String transform(@PathVariable String id) {
         return "transform";
     }
 
-    @GetMapping("/:id")
+    @GetMapping("/images/:id")
     public String getImage(@PathVariable String id) {
         return "getImage";
     }
 
-    @GetMapping
-    public String downloadImage(@PathVariable String id) {
+    @GetMapping("/images")
+    public String downloadImage() {
         return "downloadImage";
     }
 
