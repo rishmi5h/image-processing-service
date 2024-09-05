62755b92eabde7ad9a65d4ed573fd3394e943b21
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-06 05:18:14 +0530

    Update AwsS3Config.java

--- src/main/java/com/imageprocessing/config/AwsS3Config.java
+++ src/main/java/com/imageprocessing/config/AwsS3Config.java
@@ -1,23 +1,31 @@
 package com.imageprocessing.config;
 
-//@Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.regions.Region;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AwsS3Config {
 
-//    @Value("${aws.accessKey}")
-//    private String accessKey;
-//
-//    @Value("${aws.secretKey}")
-//    private String secretKey;
-//
-//    @Value("${aws.s3.region}")
-//    private String region;
-//
-//    @Bean
-//    public S3Client amazonS3Client() {
-//        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
-//        return S3Client.builder()
-//                .region(Region.of(region))
-//                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
-//                .build();
-//    }
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Bean
+    public S3Client amazonS3Client() {
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .build();
+    }
 }
