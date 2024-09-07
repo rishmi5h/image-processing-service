913376d6776231355aacec1ee2ccb9622e1417dc
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-08 02:55:43 +0530

    Update JwtService.java

--- src/main/java/com/imageprocessing/service/JwtService.java
+++ src/main/java/com/imageprocessing/service/JwtService.java
@@ -1,5 +1,6 @@
 package com.imageprocessing.service;
 
+import com.imageprocessing.entities.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -28,7 +29,11 @@ public class JwtService {
     }
 
     public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
+        Map<String, Object> claims = new HashMap<>();
+        if (userDetails instanceof User) {
+            claims.put("userId", ((User) userDetails).getId());
+        }
+        return generateToken(claims, userDetails);
     }
 
     public String generateToken(
@@ -48,6 +53,10 @@ public class JwtService {
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public Integer extractUserId(String token) {
+        return extractClaim(token, claims -> claims.get("userId", Integer.class));
     }
 
     private boolean isTokenExpired(String token) {
