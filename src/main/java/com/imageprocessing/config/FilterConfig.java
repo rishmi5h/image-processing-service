05b20c6fae0829f0159fbd80ea7910fa475a3a5f
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-15 19:57:27 +0530

    add : rate limiting

---
+++ src/main/java/com/imageprocessing/config/FilterConfig.java
@@ -0,0 +1,18 @@
+package com.imageprocessing.config;
+
+import com.imageprocessing.filter.RateLimitingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<RateLimitingFilter> rateLimitingFilter() {
+        FilterRegistrationBean<RateLimitingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RateLimitingFilter());
+        registrationBean.addUrlPatterns("/images/*");
+        return registrationBean;
+    }
+}
