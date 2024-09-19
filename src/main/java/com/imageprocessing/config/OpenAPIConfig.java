227c3af43ad7f7b0bb8f92ecd2fa7cfe31a25e2e
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-20 04:10:42 +0530

    Create OpenAPIConfig.java

---
+++ src/main/java/com/imageprocessing/config/OpenAPIConfig.java
@@ -0,0 +1,24 @@
+package com.imageprocessing.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI orderServiceAPI(){
+        return new OpenAPI()
+                .info(new Info().title("Imagery Service API")
+                        .description("Imagery Service API Description")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Imagery Service Wiki Documentation")
+                        .url("https://Imagery-service-dummy-wiki.com/docs"));
+    }
+}
